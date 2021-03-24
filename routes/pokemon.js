a265db29775ea6509b98ac79aa5f2275e77e4e14
Alan Hernandez <ingalanhernaandez@gmail.com>
2021-03-23 21:23:45 -0600

    added mysql and actualized routes

---
+++ routes/pokemon.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const pokemon = express.Router();
+const db = require('../config/database');
+
+pokemon.post("/", (req, res, next) => {
+    return res.status(200).send(req.body);
+    });
+    
+    pokemon.get("/", async (req, res, next) => {
+       const pkmn = await db.query("SELECT * FROM pokemon", );
+        return res.status(200).json(pkmn)
+    
+    })
+    
+    pokemon.get('/:id([0-9]{1,3})', (req, res, next) =>{
+        const id = req.params.id -1;
+        (id >= 0 && id <= 150) ? 
+        res.status(200).send(pke[req.params.id - 1]) :
+        res.status(200).send("Pokemón no encontrado");  
+    })
+    
+    pokemon.get('/:name([A-Za-z]+)', (req,res,next) =>{
+    const name = req.params.name;
+    const pk = pke.filter((p) => {
+        
+     return (p.name.toUpperCase() == name.toUpperCase()) && p;
+     
+    });
+    
+    (pk.length > 0) ? res.status(200).send(pk) : res.status(404).send("Pokemón no encontrado");
+    
+    })
+    
+    module.exports = pokemon;
